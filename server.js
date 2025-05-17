925799135f829b228b5de5cdb8e99bf6e23e586c
minkokolinn <minkokolinndev@gmail.com>
2025-05-17 15:28:39 +0700

    node js server side redirection

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ const server = http.createServer((req,res)=>{
     switch (req.url) {
         case "/":
             filename="home.html"
+            res.statusCode=200
             break;
         case "/about":
             filename="about.html"
+            res.statusCode=200
             break;
         case "/contact":
             filename="contact.html"
+            res.statusCode=200
+            break;
+        case "/contact-us":
+            res.statusCode=301
+            res.setHeader("Location","/contact")
             break;
         default:
             filename="404.html"
+            res.statusCode=404
             break;
     }
 
