4cf2d87e77471924f0acb411226f38fbe8e95f3a
minkokolinn <minkokolinndev@gmail.com>
2025-05-17 14:27:07 +0700

    create node http server test

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.setHeader("Content-Type","text/html")
+
+    res.write("<h1>Hello World</h1>")
+
+    res.end()
+})
+
+server.listen(3000,"localhost",()=>{
+    console.log("server listening on port 3000");
+})
