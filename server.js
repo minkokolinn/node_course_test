b906c35e1dbb7fb47c310aeb74c0b5a2657b3ace
minkokolinn <minkokolinndev@gmail.com>
2025-05-17 15:07:00 +0700

    node server basic routing

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req,res)=>{
-    res.setHeader("Content-Type","text/html")
+    let filename;
+    switch (req.url) {
+        case "/":
+            filename="home.html"
+            break;
+        case "/about":
+            filename="about.html"
+            break;
+        case "/contact":
+            filename="contact.html"
+            break;
+        default:
+            filename="404.html"
+            break;
+    }
 
-    fs.readFile("./views/home.html",(err,data)=>{
+    res.setHeader("Content-Type","text/html")
+    fs.readFile("./views/"+filename,(err,data)=>{
         if(err){
-            console.log(err);
+            console.log(err)
         }else{
-            res.write(data);
+            res.write(data)
         }
         res.end()
     });
