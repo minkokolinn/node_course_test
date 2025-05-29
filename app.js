d19362336350b91e8e71cb03ad01ceaa92e238a6
minkokolinn <minkokolinndev@gmail.com>
2025-05-30 01:45:09 +0700

    express redirecting, 404, view engine, pass data, ejs

--- app.js
+++ app.js
@@ -3,15 +3,34 @@ const app = express()
 
 console.log("express app is running")
 
+// setting up ejs view engine
+app.set("views","./views")
+app.set("view engine","ejs")
+
 app.get("/",(req,res)=>{
-    res.sendFile("./views/home.html",{root:__dirname})
+    const blogs = [
+        { title: "Title 1", intro: "This is intro of title 1"},
+        { title: "Title 2", intro: "This is intro of title 2"},
+        { title: "Title 3", intro: "This is intro of title 3"},
+    ]
+    res.render("home",{
+        blogs
+    })
 })
+
 app.get("/about",(req,res)=>{
-    res.sendFile("./views/about.html",{root:__dirname})
+    res.render("about")
 })
+
 app.get("/contact",(req,res)=>{
-    res.sendFile("./views/contact.html",{root:__dirname})
+    res.render("contact")
+})
+
+app.use((req,res)=>{
+    res.status(404).render("404")
 })
 
 
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("app is running on port 3000");
+})
