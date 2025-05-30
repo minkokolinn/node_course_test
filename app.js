65332b89eda4e06abfcd54bb21d81c4e2ef96bc7
minkokolinn <minkokolinndev@gmail.com>
2025-05-31 01:59:52 +0700

    express middleware, third party middleware and static middleware setup

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-console.log("express app is running")
+const morgan = require("morgan")
+
+console.log("express app is running");
 
 // setting up ejs view engine
-app.set("views","./views")
-app.set("view engine","ejs")
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    const blogs = [
-        { title: "Title 1", intro: "This is intro of title 1"},
-        { title: "Title 2", intro: "This is intro of title 2"},
-        { title: "Title 3", intro: "This is intro of title 3"},
-    ]
-    res.render("home",{
-        blogs
-    })
-})
+const logger = (env) => {
+  return (req, res, next) => {
+    if(env === "dev"){
+      console.log(`${req.method} ${req.originalUrl} ---`);
+    }
+    next();
+  };
+};
 
-app.get("/about",(req,res)=>{
-    res.render("about")
-})
+app.use(morgan('dev'));
 
-app.get("/contact",(req,res)=>{
-    res.render("contact")
-})
+app.use(express.static("public"))
 
-app.use((req,res)=>{
-    res.status(404).render("404")
-})
+app.get("/", (req, res) => {
+  const blogs = [
+    { title: "Title 1", intro: "This is intro of title 1" },
+    { title: "Title 2", intro: "This is intro of title 2" },
+    { title: "Title 3", intro: "This is intro of title 3" },
+  ];
+  res.render("home", {
+    blogs,
+    title: "Home",
+  });
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("app is running on port 3000");
-})
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Contact",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", {
+    title: "404 Not Found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
